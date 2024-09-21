36611baade929f6030ae39df9e843faafae737b4
Vikram.Dhanapalan <vikram.the.coder@gmail.com>
2024-09-21 21:17:17 +0530

    Homepage set for the project

--- ui/views.py
+++ ui/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
+def homepage(req):
+    if req.method == 'GET':
+        return render(request=req,
+                      template_name='index.html')
+    raise Http404("Invalid Request")
