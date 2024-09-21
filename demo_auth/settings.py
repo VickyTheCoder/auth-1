fe13179f129f780d296ca15ed4977be63d05414a
Vikram.Dhanapalan <vikram.the.coder@gmail.com>
2024-09-21 20:58:29 +0530

    Added Ui & Customer apps

--- demo_auth/settings.py
+++ demo_auth/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ui.apps.UiConfig',
+    'customer.apps.CustomerConfig',
 ]
 
 MIDDLEWARE = [
